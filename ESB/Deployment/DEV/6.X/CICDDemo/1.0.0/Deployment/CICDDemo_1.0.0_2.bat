112d1c12a8be8042f1ad9960f4f4ff0d8f86b76c
madhulathachilakala <madhulatha.ch@gmail.com>
2018-05-22 18:10:28 +0000

    Pushing deployment artifacts to local

---
+++ ESB/Deployment/DEV/6.X/CICDDemo/1.0.0/Deployment/CICDDemo_1.0.0_2.bat
@@ -0,0 +1,4 @@
+
+create application CICDDemo.application CICDDemo,.WebResources,.settings,META-INF,Processes,Resources,Schemas,Service Descriptors C:/GITWorkspace/CICDDemo/output/ESB/Development/6.X/ApplicationModules/CICDDemo/trunk/Source/CICDDemo
+export -name CICDDemo.application_1.0.0 force -ear CICDDemo.application C:/GITWorkspace/CICDDemo/output/ESB/Deployment/DEV/6.X/CICDDemo/1.0.0/Deployment/EAR
+exit
